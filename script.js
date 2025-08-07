747a56cadab92e5311cf4f3cce9507f780b9a73f
giselleS08 <giselle.sarabia8@gmail.com>
2025-08-07 14:31:41 +0000

    colors

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ let startButtonText = `
   bbb`;
 
   let palette1 = {
-    b: color(51,56,99),//dark
+    b: color(51,56,99),//outline more than likely orange
 
-    s:color(118,188,239),//cyanlike
-    d:color(86,149,234),//neutral blue
-    f:color(55,96,193)//gray/dark blue
+    s:color(118,188,239),//this will be the lighter orange
+    d:color(86,149,234),//yellow main color
+    f:color(55,96,193)//word color like black for visibility
   };
         startButton = new Sprite();
         startButton.img = spriteArt(startButtonText, 20, palette1);
