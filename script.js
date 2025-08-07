ba860f3a0c86e7e2f9f21912a3138315471c3b95
giselleS08 <giselle.sarabia8@gmail.com>
2025-08-07 19:17:23 +0000

    flipp colors

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ let startButtonText = `
   `;
 
   let palette1 = {
-    b: color(225,86,22),//outline more than likely orange
+    s: color(225,86,22),//outline more than likely orange
 
-    s:color(204,0,0),//this will be the lighter orange
+    b:color(204,0,0),//this will be the lighter orange
     d:color(86,149,234),//yellow main color
     f:color(55,96,193)//word color like black for visibility
   };
