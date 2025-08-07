292eb7815bd00b0dd230263cb5973eb0cb111b90
giselleS08 <giselle.sarabia8@gmail.com>
2025-08-07 19:23:34 +0000

    final color

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let startButtonText = `
     b: color(175,0,0),//outline more than likely orange
 
     s:color(255,25,25),//this will be the lighter orange
-    d:color(86,149,234),//yellow main color
+    d:color(255,191,0),//yellow main color
     f:color(255,93,0)//word color like black for visibility
   };
         startButton = new Sprite();
