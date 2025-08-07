91fabd8966854c7d55b73cf1dd553f2fdc4082df
giselleS08 <giselle.sarabia8@gmail.com>
2025-08-07 11:30:15 +0000

    to know how to fix sprite

--- script.js
+++ script.js
@@ -9,8 +9,21 @@ startScreen();
 
 
 //making a button for start
-startbutton = new Sprite(400, 600, 50, 50, "k");
-}
+let horchataText = `
+  bbb`;
+
+  let palette1 = {
+    b: color(51,56,99),//dark
+
+    s:color(118,188,239),//cyanlike
+    d:color(86,149,234),//neutral blue
+    f:color(55,96,193)//gray/dark blue
+  };
+        horchata = new Sprite();
+        horchata.img = spriteArt(horchataText, 20, palette1);
+       horchata.pos={x:115, y: 75};
+        horchata.rotation =0;
+        horchata.collider = "kinematic";
 
 function draw() {
   //
